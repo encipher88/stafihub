f46998f8aa7438e01fdd715d336963f429fc8c54
encipher <36136421+encipher88@users.noreply.github.com>
2022-05-13 18:30:12 +0300

    Update recoverkey.sh

--- recoverkey.sh
+++ recoverkey.sh
@@ -3,7 +3,7 @@
 	apt install expect
 	sleep 2
 		if [[ ! $mnemonic ]]; then
-		read -p "Enter bip39 mnemonic: " mnemonic
+		read -r -p "Enter bip39 mnemonic: " var1 var2 var3 var4 var5 var6 var7 var8 var9 var10 var11 var12 var13 var14 var15 var16 var17 var18 var19 var20 var21 var22 var23 var24 "
 		echo 'export mnemonic='${mnemonic} >> $HOME/.bash_profile
 	fi
 	source $HOME/.bash_profile
