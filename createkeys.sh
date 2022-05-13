78ab60c3dfa82f49de67103409135d8036146d46
encipher <36136421+encipher88@users.noreply.github.com>
2022-05-13 17:02:44 +0300

    Create createkeys.sh

---
+++ createkeys.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+	echo -e "\n\e[42mPreparing to create wallet...\e[0m\n" && sleep 1
+	apt install expect
+	sleep 2
+sudo tee <<EOF >/dev/null $HOME/.stafihub/stafihub_add_key.sh 
+#!/usr/bin/expect -f
+EOF
+echo "set timeout -1
+spawn stafihubd keys add $WALLET --home $HOME/.stafihub
+match_max 100000
+expect -exact \"Enter keyring passphrase:\"
+send -- \"$PASSWORD\r\"
+expect -exact \"\r
+Re-enter keyring passphrase:\"
+send -- \"$PASSWORD\r\"
+expect eof" >> $HOME/.stafihub/stafihub_add_key.sh
+sudo chmod +x $HOME/.stafihub/stafihub_add_key.sh
+$HOME/.stafihub/stafihub_add_key.sh &>> $HOME/.stafihub/$WALLET.txt
+
+echo -e "You can find your mnemonic by the following command:"
+echo -e "\e[32mcat $HOME/.stafihub/$WALLET.txt\e[39m"
+
+export WALLET_ADDRESS=`cat $HOME/.stafihub/$WALLET.txt | grep address | awk '{split($0,addr," "); print addr[2]}' | sed 's/.$//'`
+echo 'export WALLET_ADDRESS='${WALLET_ADDRESS} >> $HOME/.bash_profile
+. $HOME/.bash_profile
+echo -e '\n\e[45mYour wallet address:' $WALLET_ADDRESS '\e[0m\n'
+
+cd $HOME
+
+sleep 5
+
+sudo tee <<EOF >/dev/null $HOME/.stafihub/stafihub_add_valkey.sh 
+#!/usr/bin/expect -f
+EOF
+echo "set timeout -1
+spawn stafihubd keys show $WALLET --bech val -a
+match_max 100000
+expect -exact \"Enter keyring passphrase:\"
+send -- \"$PASSWORD\r\"
+expect -exact \"\r
+Re-enter keyring passphrase:\"
+send -- \"$PASSWORD\r\"
+expect eof" >> $HOME/.stafihub/stafihub_add_valkey.sh
+sudo chmod +x $HOME/.stafihub/stafihub_add_valkey.sh
+$HOME/.stafihub/stafihub_add_valkey.sh &>> $HOME/.stafihub/valop.txt
+
+export VALOPER_ADDRESS=`cat $HOME/.stafihub/valop.txt | grep '^stafivaloper' | awk '{print$1}' | sed 's/.$//'`
+echo 'export VALOPER_ADDRESS='${VALOPER_ADDRESS} >> $HOME/.bash_profile
+. $HOME/.bash_profile
+echo -e '\n\e[45mYour wallet address:' $VALOPER_ADDRESS '\e[0m\n'
+
+cd $HOME
